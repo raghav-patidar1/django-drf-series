27caf3fe9a9ceb59cdc38904f241445bd287723d
Raghav patidar <raghav.patidar.42@gmail.com>
2025-06-27 18:31:56 +0530

    feat: Create ProductSerializer with validation and basic endpoints.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import ProductSerializer
+from .models import Product
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
