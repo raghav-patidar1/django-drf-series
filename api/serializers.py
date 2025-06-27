e739ec16a56e4cdef69689f30db85613b527ff3a
Raghav patidar <raghav.patidar.42@gmail.com>
2025-06-27 21:38:40 +0530

    feat: Create serializers for Order and OrderItem along with an endpoint /orders/

--- api/serializers.py
+++ api/serializers.py
@@ -12,5 +12,39 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than 0.")
         return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        source='product.price',
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ('product_name', 'product_price', 'quantity', 'item_subtotal')
+
+
+class OrderSerializer(serializers.ModelSerializer):
     
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id', 
+            'user', 
+            'created_at',
+            'status', 
+            'items', 
+            'total_price'
+        )
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
 
--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import ProductSerializer, OrderSerializer
+from .models import Product, Order, OrderItem
 
 
 @api_view(['GET'])
@@ -20,3 +20,8 @@ def product_detail(request, pk):
     return Response(serializer.data)
 
 
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
